765314aa24f7cd7e50aeba68c5b5292a5fb188cb
naveensaipadavala <naveennaidu3399@gmail.com>
2026-03-11 23:57:14 +0530

    second commit

--- src/main/java/com/example/SampleGitDemo/SampleGitDemoApplication.java
+++ src/main/java/com/example/SampleGitDemo/SampleGitDemoApplication.java
@@ -2,12 +2,19 @@ package com.example.SampleGitDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SampleGitDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleGitDemoApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello";
+	}
 }
