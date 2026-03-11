4f00322b4c667b446fdb5c9950b0c02a2fe3fd85
naveensaipadavala <naveennaidu3399@gmail.com>
2026-03-11 23:36:19 +0530

    first commmit

---
+++ src/test/java/com/example/SampleGitDemo/SampleGitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SampleGitDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleGitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
